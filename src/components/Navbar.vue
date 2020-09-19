0cae952e905ca580d4be8a25615dabe71f5c9fd7
reskuy <48839288+reskuy@users.noreply.github.com>
2020-09-19 13:46:52 +0700

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+<v-container
+      class="overflow-hidden"
+    >
+    <!-- NavBar Atas -->
+    <Appbar/>
+  <!--NAV kiri -->
+   <Navdrawer/>   
+</v-container>
+</template>
+
+<script>
+import Appbar from '@/views/Appbar'
+import Navdrawer from '@/views/Navdrawer'
+export default {
+  components: {
+    Appbar,
+    Navdrawer
+  },data() {
+    return {
+      
+    }
+  },
+}
+</script>
