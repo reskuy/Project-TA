7170dc85ac02f15c784d5482a6580ccce0be4638
reskuy <48839288+reskuy@users.noreply.github.com>
2020-09-23 10:43:22 +0700

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Barang from "@/components/Barang"
 
+
+
+Vue.use(VueRouter);
+
+//const router = new VueRouter({
+  export default new VueRouter({
+    routes : [
+      {path:'/Barang',name:'Barang',component: Barang}
+    ],
+    mode: 'history'
+  });
+
+  
