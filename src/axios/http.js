61ccbe3c1243f87ad7409d18174930088c4c9af4
#48839288 <reski.naksmd@gmail.com>
2020-12-06 23:52:21 +0700

    updt

--- src/axios/http.js
+++ src/axios/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 export default axios.create({
-  baseURL: "http://192.168.1.177:8000/api",
+  baseURL: "https://laravel-ta.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   }
